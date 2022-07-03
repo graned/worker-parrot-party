54438d29b7233ef25ad4e35ba8c1adb8c53ca424
Eduardo Anaya <gran_ed@hotmail.com>
2022-07-03 10:23:35 +0200

    Feature/enchance package publishing (#3)
    * add pretter & create publishing commands
    
    * fix linting follwing prettier

--- samples/complex-task.ts
+++ samples/complex-task.ts
@@ -1,43 +1,49 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-console */
+/* eslint-disable no-plusplus */
+/* eslint-disable no-await-in-loop */
 import { PoolParty, PoolPartyConfig } from '../src/PoolParty'
 import 'isomorphic-fetch'
 
 async function fetchToSite(url: string): Promise<Response> {
-    return fetch(url);
+  return fetch(url)
 }
 
 async function pageFetcher(urlsToFetch: Array<string>): Promise<void> {
-    for (const url of urlsToFetch) {
-        const response = await fetchToSite(url)
-        console.log(response.status)
-        console.log(await response.json())
-    }
+  for (const url of urlsToFetch) {
+    const response = await fetchToSite(url)
+    console.log(response.status)
+    console.log(await response.json())
+  }
 }
 
 const poolPartyConfig: PoolPartyConfig = {
-    partySize: 2,
-    basePath: __dirname,
-    libraryDeclaration: [
-        {
-            name: 'isomorphic-fetch',
-            importDeclaration: ''
-        }
-    ],
-    helpers: [fetchToSite],
-    task: pageFetcher,
-    onSuccess: () => { console.log('all good in the hood!') },
-    onError: (error: any) => { console.log('error', error) }
+  partySize: 2,
+  basePath: __dirname,
+  libraryDeclaration: [
+    {
+      name: 'isomorphic-fetch',
+      importDeclaration: '',
+    },
+  ],
+  helpers: [fetchToSite],
+  task: pageFetcher,
+  onSuccess: () => {
+    console.log('all good in the hood!')
+  },
+  onError: (error: unknown) => {
+    console.log('error', error)
+  },
 } as PoolPartyConfig
 
 const poolParty: PoolParty = new PoolParty(poolPartyConfig)
 
 // nicolas cage meme pages
-const nicolasCageUrls: Array<string> = [
-    'https://www.dw.com/de/themen/s-9077',
-]
+const nicolasCageUrls: Array<string> = ['https://www.dw.com/de/themen/s-9077']
 
 // nicolas cage meme pages
 const memesUrls: Array<string> = [
-    'https://www.pocket-lint.com/de-de/software/news/152027-diese-meme-existieren-nicht-und-werden-von-ai-erstellt',
+  'https://www.pocket-lint.com/de-de/software/news/152027-diese-meme-existieren-nicht-und-werden-von-ai-erstellt',
 ]
 
 poolParty.run([nicolasCageUrls])
--- src/CustomErrors.ts
+++ src/CustomErrors.ts
@@ -1,20 +1,20 @@
+/* eslint-disable max-classes-per-file */
 export class CustomErrors extends Error {
-    constructor(private readonly _message: string, private readonly _type: string) {
-        super(_message)
-    }
+  constructor(private readonly _message: string, private readonly _type: string) {
+    super(_message)
+  }
 
-    get type(): string {
-        return this._type
-    }
+  get type(): string {
+    return this._type
+  }
 
-    get message(): string {
-        return this._message
-    }
+  get message(): string {
+    return this._message
+  }
 }
 
 export class NoIdleParrotError extends CustomErrors {
-    constructor(msg: string) {
-        super(msg, 'NO_IDLE_PARROT_ERROR')
-    }
+  constructor(msg: string) {
+    super(msg, 'NO_IDLE_PARROT_ERROR')
+  }
 }
-
